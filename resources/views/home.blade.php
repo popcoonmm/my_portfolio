dbba7fc148582538dec5b4fa746d60d9ab6c7a81
EC2 Default User <ec2-user@ip-172-31-21-171.us-east-2.compute.internal>
2019-08-14 08:23:10 +0000

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+　　<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    </head>
+    
+    <body>
+        <header>
+            
+        </header>
+        
+        <h1>myprofile</h1>
+        <p>Yuna Miyasaka</p>
+         <div class="links">
+                    <a href="https://laravel.com/docs">CAKEHOUSE</a>
+                    <a href="https://www.instagram.com/yuu.nna/?hl=ja">instagram</a>
+                    <a href="https://twitter.com/yuna_miyaa">Twitter</a>
+                    <a href="https://github.com/popcoonmm/cakehouse">GitHub</a>
+                </div>
+        
+    </body>
+</html>
